ac5e8b2c85a03e028b7fe607f88dee83d3ae0b1e
Shahriar Ahmmed <shahariarahmmed.sa@gmail.com>
2022-10-17 00:11:39 +0600

    Headers section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import Header from "./Header/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Dev Skill Test</h1>
+    <div>
+      <Header></Header>
     </div>
+    
   );
 }
 
---
+++ src/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#" className='text-primary fw-bold'>Dev Skill Test</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#action2" className='fw-bold'>Quiz Topics</Nav.Link>
+                        <Nav.Link href="#action2" className='fw-bold'>Statistics</Nav.Link>
+                        <Nav.Link href="#action2" className='fw-bold'>Blog</Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
